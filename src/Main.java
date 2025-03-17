c5328ad11fb7609522793ac31137875a2d6f300e
Lusine <lusinehyan6@gmail.com>
2025-03-17 10:32:59 +0400

    4/03/25 homework

--- src/Main.java
+++ src/Main.java
@@ -6,3 +6,17 @@ public class Main {
 
     }
 }
+//homework
+import java.util.List;
+public class Main
+{ public static void findFirstOddGreaterThan10(List<Integer> numbers)
+{ for (int num : numbers) {
+    if (num > 0 && num > 10 && num % 2 == 1)
+    {
+        System.out.println(num); return;
+
+    } } System.out.println("Չկա համապատասխան թիվ");
+} public static void main(String[] args) { List<Integer> numbers = List.of(3, -5, 8, 12, 15, 7, 21);
+    findFirstOddGreaterThan10(numbers);
+
+} }
